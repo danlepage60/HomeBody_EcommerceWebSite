9bff25c889f9c160698e6c017802080a6b9620a1
Daniel Lepage <danlepage@gmail.com>
2020-11-28 12:56:32 -0700

    first push

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $10",
+        "array": [0, 10]
+    },
+    {
+        "_id": 2,
+        "name": "$11 to $20",
+        "array": [11, 20]
+    },
+    {
+        "_id": 3,
+        "name": "$21 to $50",
+        "array": [21, 50]
+    },
+    {
+        "_id": 4,
+        "name": "$51 to $100",
+        "array": [51, 100]
+    },
+    {
+        "_id": 5,
+        "name": "More than $100",
+        "array": [101, 1500000]
+    }
+]
+
+
+
+const products = [
+    {
+        "_id": 1,
+        "name": "Blankets"
+    },
+    {
+        "_id": 2,
+        "name": "Candles"
+    },
+    {
+        "_id": 3,
+        "name": "Cozy Socks"
+    },
+    {
+        "_id": 4,
+        "name": "Lounge Sets"
+    },
+    {
+        "_id": 5,
+        "name": "Meditation Pillows"
+    },
+    {
+        "_id": 6,
+        "name": "Sleeping Masks"
+    },
+    {
+        "_id": 7,
+        "name": "Slippers"
+    }
+]
+
+
+export {
+    price,
+    products
+}
